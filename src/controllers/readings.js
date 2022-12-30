ef7b3513fe9cb22fb7d65928d1402c592a02cb5b
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-12-30 23:17:15 +0200

    Add Ex13.22: change read status with PUT

--- src/controllers/readings.js
+++ src/controllers/readings.js
@@ -3,16 +3,32 @@ const { Reading, UserReading } = require('../models')
 const { tokenExtractor, AuthorizationError } = require('../middleware')
 const router = express.Router()
 
+const checkUserPrivilege = (token, resourceUserId) => {
+  if (token.id !== resourceUserId) {
+    throw new AuthorizationError(`Unauthorized user access`)
+  }
+}
+
 // POST, add a new reading
 router.post('/', tokenExtractor, async (req, res) => {
-  if (req.decodedToken.id !== req.body.userId) {
-    throw new AuthorizationError(`Unauthorized addition to user's reading list`)
-  }
+  checkUserPrivilege(req.decodedToken, req.body.userId)
 
   const reading = Reading.build({
     ...req.body,
     read: false,
   })
+  await reading.save()
+
+  res.json(reading)
+})
+
+// PUT, change read status
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const reading = await Reading.findByPk(req.params.id)
+
+  checkUserPrivilege(req.decodedToken, reading.userId)
+
+  reading.read = req.body.read
   await reading.save()
 
   res.json(reading)
